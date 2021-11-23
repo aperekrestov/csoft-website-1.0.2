31f4970823b682e1bc3875980c39b31c72401415
Alex <a.perekrestov@yandex.ru>
2021-11-23 17:02:14 +0300

    Вёрстка блоками
    Обновил верстку контента блоками

--- #src-code/js/map.js
+++ #src-code/js/map.js
@@ -3,25 +3,25 @@ if (document.querySelector('.map__ineractive-container')) {
 
 	const MAP = document.querySelector('.map__bg')
 	let currentScale = 0
-	let currentScaleY = 0
+	// let currentScaleY = 0
 	let officeInfo = document.querySelector('.map__office-info-cont')
 	let targets = document.querySelectorAll('.icon-target')
 
 	const KALININGRAD = document.getElementById('kaliningrad')
-	const PETERBURG = document.getElementById('petersburg')
+	const SANKT_PETERBURG = document.getElementById('sankt-peterburg')
 	const KRASNODAR = document.getElementById('krasnodar')
 	const VOLGOGRAD = document.getElementById('volgograd')
-	const MOSCOW = document.getElementById('moscow')
+	const MOSKVA = document.getElementById('moskva')
 	const VORONEZH = document.getElementById('voronezh')
 	const IVANOVO = document.getElementById('ivanovo')
-	const NIZHNIY_NOVGOROD = document.getElementById('nizhny-novgorod')
+	const NIZHNIJ_NOVGOROD = document.getElementById('nizhnij-novgorod')
 	const KAZAN = document.getElementById('kazan')
 	const SAMARA = document.getElementById('samara')
 	const TYMEN = document.getElementById('tyumen')
 	const OMSK = document.getElementById('omsk')
 	const NOVOSIBIRSK = document.getElementById('novosibirsk')
 	const KRASNOYARSK = document.getElementById('krasnoyarsk')
-	const KHABAROVSK = document.getElementById('khabarovsk')
+	const HABAROVSK = document.getElementById('habarovsk')
 
 	window.addEventListener('load', e => {
 		calculation()
@@ -66,19 +66,19 @@ if (document.querySelector('.map__ineractive-container')) {
 		currentScale = document.querySelector('.map__wrapper').clientWidth / (1740 - 36 * 2) //! учитываем значение 36 - это отступ по краям карты
 
 		gsap.set(KALININGRAD, { left: 37 * currentScale - 8, top: 190 * currentScale - 16 })
-		gsap.set(PETERBURG, { left: 280 * currentScale - 8, top: 175 * currentScale - 16 })
+		gsap.set(SANKT_PETERBURG, { left: 280 * currentScale - 8, top: 175 * currentScale - 16 })
 		gsap.set(KRASNODAR, { left: 60 * currentScale - 8, top: 355 * currentScale - 16 })
 		gsap.set(VOLGOGRAD, { left: 200 * currentScale - 8, top: 335 * currentScale - 16 })
-		gsap.set(MOSCOW, { left: 200 * currentScale - 8, top: 275 * currentScale - 16 })
+		gsap.set(MOSKVA, { left: 200 * currentScale - 8, top: 275 * currentScale - 16 })
 		gsap.set(VORONEZH, { left: 190 * currentScale - 8, top: 305 * currentScale - 16 })
 		gsap.set(IVANOVO, { left: 260 * currentScale - 8, top: 235 * currentScale - 16 })
-		gsap.set(NIZHNIY_NOVGOROD, { left: 310 * currentScale - 8, top: 255 * currentScale - 16 })
+		gsap.set(NIZHNIJ_NOVGOROD, { left: 310 * currentScale - 8, top: 255 * currentScale - 16 })
 		gsap.set(KAZAN, { left: 380 * currentScale - 8, top: 285 * currentScale - 16 })
 		gsap.set(SAMARA, { left: 450 * currentScale - 8, top: 355 * currentScale - 16 })
 		gsap.set(TYMEN, { left: 660 * currentScale - 8, top: 405 * currentScale - 16 })
 		gsap.set(OMSK, { left: 850 * currentScale - 8, top: 520 * currentScale - 16 })
 		gsap.set(NOVOSIBIRSK, { left: 950 * currentScale - 8, top: 470 * currentScale - 16 })
 		gsap.set(KRASNOYARSK, { left: 1105 * currentScale - 8, top: 450 * currentScale - 16 })
-		gsap.set(KHABAROVSK, { left: 1450 * currentScale - 8, top: 500 * currentScale - 16 })
+		gsap.set(HABAROVSK, { left: 1450 * currentScale - 8, top: 500 * currentScale - 16 })
 	}
 }
